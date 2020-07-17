3b41387f2ad30dd8808246dbb320b17a6c08166d
root <root@6e975a9fff26>
2020-07-17 23:59:35 +0000

    Completed PentagonCrazy, FlamingNinjaStar, Greeter, WorldDomination, and MadlibsOfTheAmazon.

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -8,43 +8,44 @@ import org.jointheleague.graphical.robot.Robot;
 import java.awt.Color;
 
 public class PentagonCrazy {
-	
+
 	private void drawPattern() {
 		
 		//  Create a new Robot
-
+		Robot rob = new Robot();
 		//  Put the robot's pen down
-
+		rob.penDown();
 		//  SPEED. Make the robot go at maximum speed (100)
-
+		rob.setSpeed(100);
 		//  COLOR. Set the pen to a color that you like for the shape
-
+		rob.setPenColor(Color.cyan);
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape has.
 		//  				(Hint: its called PentagonCrazy)
-
+		int Sides=5;
 		//  TURN ANGLE. Make another int variable for the angle you want the robot to turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has.
-
+		int Angle=360/Sides;
 		//  LOOP. Use a for loop to repeat ALL the following lines of code 200 times. 
-		
+		for(int i=0; i<200; i++) {
 				//  DRAW.  Make the robot move "i" pixels
 				//         "i" is the variable in the for loop
-		
+				rob.move(i);
 				//  TURN.  Turn the robot the amount in your angle variable + 1
-
-
-		// Hide the robot so you can see the pattern.
+				rob.turn(Angle+1);
+		}
 		
-		// Check the pattern against the picture in the recipe. If it matches, you are done!
+		rob.hide();
 	}
+	// Hide the robot so you can see the pattern.
 
-	
+	// Check the pattern against the picture in the recipe. If it matches, you are
+	// done!
+
 	// Variations:
 	// *12. make the pattern really huge
-	// *13. randomize the color of the pattern 
+	// *13. randomize the color of the pattern
 	// *14. experiment with different shapes
 
-	
 	public static void main(String[] args) {
 		new PentagonCrazy().drawPattern();
 	}
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String question = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if(question.equals("yes")) {
+			System.out.println("You will rule the world!");
+		}else {
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+			System.out.println("Then you better sign up for classes at The League!");
+		}
 	}
 }
 
